03c4e8551603ec88b6c257f25f9ff06cd395cfcd
Matthew Gribbins <gribbinsmatthew@gmail.com>
2025-08-23 00:52:54 -0700

    Adding the ugliest scoreboard of all time :)

--- pages/history/season_25_26.js
+++ pages/history/season_25_26.js
@@ -3,8 +3,97 @@ import utilStyles from "../../styles/utils.module.css";
 import Layout from "../../components/layout";
 import Link from "next/link";
 import { GetExtendedLeagueTable } from "../../lib/history_util";
+import { Matchups, calculateDraftManagerScore } from "../../lib/matchups";
+import * as managers from "../../data/managers.json";
 
-export default function Season_25_26({}) {
+export async function getServerSideProps() {
+  const LEAGUE_A_ID = 157;
+  const LEAGUE_B_ID = 461;
+
+  const boostrapRes = await fetch(
+    "https://fantasy.premierleague.com/api/bootstrap-static/"
+  );
+  const bootstrapData = await boostrapRes.json();
+
+  const currentGameweekObject = bootstrapData.events.find(
+    (event) => event.is_current === true
+  );
+
+  const gameweekId = currentGameweekObject
+    ? currentGameweekObject.id
+    : bootstrapData.events.find((event) => event.is_next === true)?.id || 1;
+
+  const liveRes = await fetch(
+    `https://fantasy.premierleague.com/api/event/${gameweekId}/live/`
+  );
+  const liveData = await liveRes.json();
+  const playerScoreMap = new Map();
+  liveData.elements.forEach((player) => {
+    playerScoreMap.set(player.id, player.stats.total_points);
+  });
+
+  const matchupsARes = await fetch(
+    `https://draft.premierleague.com/api/league/${LEAGUE_A_ID}/details`
+  );
+  const matchupsAData = await matchupsARes.json();
+  const matchupsBRes = await fetch(
+    `https://draft.premierleague.com/api/league/${LEAGUE_B_ID}/details`
+  );
+  const matchupsBData = await matchupsBRes.json();
+
+  const currentWeekAMatches = matchupsAData.matches.filter(
+    (match) => match.event === gameweekId
+  );
+
+  const currentWeekBMatches = matchupsBData.matches.filter(
+    (match) => match.event === gameweekId
+  );
+
+  var final = currentWeekAMatches.concat(currentWeekBMatches);
+
+  const processedMatchups = await Promise.all(
+    final.map(async (match) => {
+      const manager1Id = match.league_entry_1;
+      const manager2Id = match.league_entry_2;
+
+      const score1 = await calculateDraftManagerScore(
+        managers[manager1Id].entry_id,
+        gameweekId,
+        playerScoreMap
+      );
+      const score2 = await calculateDraftManagerScore(
+        managers[manager2Id].entry_id,
+        gameweekId,
+        playerScoreMap
+      );
+
+      const matchId = gameweekId + "-" + manager1Id + "-" + manager2Id;
+
+      return {
+        id: matchId,
+        manager1: {
+          id: manager1Id,
+          managerName: managers[manager1Id].name,
+          liveScore: score1,
+        },
+        manager2: {
+          id: manager2Id,
+          managerName: managers[manager2Id].name,
+          liveScore: score2,
+        },
+      };
+    })
+  );
+
+  return {
+    props: {
+      processedMatchups: processedMatchups,
+      gameweekId: gameweekId,
+    },
+  };
+}
+
+export default function Season_25_26({ processedMatchups, gameweekId }) {
   return (
     <Layout history>
       <div className={utilStyles.main}>
@@ -24,15 +113,10 @@ export default function Season_25_26({}) {
         <br />
         <br />
         <br />
-        {/* <button class="accordion">Section 1</button>
-        <div class="panel">
-          <p>Test words</p>
-        </div>
-        <button class="accordion">Section 2</button>
-        <div class="panel">
-          <p>More test words</p>
-        </div>
-        <br /> */}
+        <Matchups
+          processedMatchups={processedMatchups}
+          gameweekId={gameweekId}
+        />
         <h2>League B Table</h2>
         <div>
           <GetExtendedLeagueTable range="'League Tables'!AH14:AR26" />
