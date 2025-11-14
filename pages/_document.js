564381bcacd7b7ff7c1f748fab644f66ea3ae0f8
Matthew Gribbins <gribbinsmatthew@gmail.com>
2025-11-13 21:13:42 -0800

    Fix some other metadata.

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="application-name" content="Game of Stones" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
