02346ae81a8115b6601c843d2530a10cc6600848
Matthew Gribbins <gribbinsmatthew@gmail.com>
2026-08-14 12:54:00 -0700

    [Fantasy] Add a carousel to the LeaguePreviewCard display.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "@mantine/core/styles.css";
+import "@mantine/carousel/styles.css";
 
 import Head from "next/head";
 import { createTheme, MantineProvider, AppShell } from "@mantine/core";
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -5,35 +5,69 @@ import {
   SimpleGrid,
   Skeleton,
   Text,
+  Box,
 } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
 import { FantasyAuth } from "../components/Auth/FantasyAuth";
 import { ManagerProvider, useManager } from "../contexts/ManagerContext";
 import { TeamPreviewCard } from "../components/Team/TeamPreviewCard";
 import { LeaguePreviewCard } from "../components/League/LeaguePreviewCard";
 import { useManagerLeagues } from "../hooks/useManagerLeagues";
+import { useLeagues } from "../hooks/useLeagues";
+import { useSingleLeagueForManager } from "../hooks/useSingleLeagueForManager";
 
 function LeagueCards() {
   const { manager, supabase } = useManager();
-  const { data: leagues, error } = useManagerLeagues(manager, supabase);
+  const { data: managerLeague, error: managerLeagueError } =
+    useSingleLeagueForManager(manager, supabase);
+  const { data: leagues, error } = useLeagues(supabase);
 
+  if (managerLeagueError) return <Text c="red">{managerLeagueError}</Text>;
   if (error) return <Text c="red">{error}</Text>;
   if (!leagues) return <Skeleton height={200} />;
 
-  return leagues.map((league) => (
-    <LeaguePreviewCard
-      key={league.id}
-      leagueId={league.id}
-      leagueName={league.name}
-      supabase={supabase}
-    />
-  ));
+  const defaultLeagueId = managerLeague?.id;
+
+  const initialSlideIndex = Math.max(
+    0,
+    leagues.findIndex((league) => league.id === defaultLeagueId),
+  );
+
+  return (
+    <Box maw={380} w="98vw">
+      <Carousel
+        withIndicators
+        height="100%"
+        width="100%"
+        slideGap="md"
+        align="center"
+        withIndicators={false}
+        controlsOffset={2}
+        controlSize={14}
+        initialSlide={initialSlideIndex}
+        emblaOptions={{
+          loop: true,
+        }}
+      >
+        {leagues.map((league) => (
+          <Carousel.Slide key={league.id}>
+            <LeaguePreviewCard
+              leagueId={league.id}
+              leagueName={league.name}
+              supabase={supabase}
+            />
+          </Carousel.Slide>
+        ))}
+      </Carousel>
+    </Box>
+  );
 }
 
 export default function Dashboard({ allPostsData, featuredPost }) {
   return (
     <>
       <ManagerProvider>
-        <Container fluid>
+        <Container fluid px={0}>
           <Stack align="center">
             <Title ta="center">Game of Stones Season 5</Title>
             <Text fw={700}>Draft Times</Text>
