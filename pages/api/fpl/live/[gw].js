456ff6945a341c8de370e1ea6625e7e96c142bac
Matthew Gribbins <gribbinsmatthew@gmail.com>
2026-08-21 11:14:19 -0700

    [Fantasy] Lower maxage of call data.

--- pages/api/fpl/live/[gw].js
+++ pages/api/fpl/live/[gw].js
@@ -9,10 +9,7 @@ export default async function handler(req, res) {
 
   try {
     const liveData = await getLiveData(gw);
-    res.setHeader(
-      "Cache-Control",
-      "s-maxage=31536000, stale-while-revalidate=30",
-    );
+    res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate=30");
     return res.status(200).json(liveData);
   } catch (error) {
     return res.status(405).json({ error: "Method not allowed" });
